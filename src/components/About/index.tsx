b07933e19014fc72ee40cd0552df9734374b9736
Hugo <hugopatricio51@gmail.com>
2023-01-06 12:29:15 -0300

    fix: improving responsiveness

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -3,57 +3,57 @@ import { Sidebar } from '../Sidebar'
 
 export const About = () => {
     return (
-        <Flex bg='#000'>
+        <Flex bg='#000' h='100vh'>
             <Sidebar />
             <SimpleGrid flex='1' gap='4' minChildWidth='320px' alignItems='flex-start' m='auto 0'>
-                <Box padding={["6", "8"]} borderRadius='8' pb='4' bg='#101213' h='30vh' ml='4'>
-                    <Text fontWeight='extrabold' fontSize='2xl' mb='4' color='#0befda' align='center' fontFamily='Open Sans, sans-serif'>
+                <Box padding={["6", "8"]} borderRadius='8' pb='4' bg='#101213' h='100%' ml='4'>
+                    <Text fontWeight='extrabold' fontSize={["sm", "md", "lg", "2xl"]} mb='4' color='#0befda' align='center' fontFamily='Open Sans, sans-serif'>
                         Sobre
                     </Text>
 
-                    <Text fontWeight='bold' fontSize='4xs' mb='4' color='white' align='justify' fontFamily='Open Sans, sans-serif'>
+                    <Text fontWeight='bold' fontSize={["xs", "2xs", "3xs", "lg"]} mb='4' color='white' align='justify' fontFamily='Open Sans, sans-serif'>
                         Desde o primeiro contato com um computador aos 8 anos de idade já sabia que aquela máquina iria transformar minha vida, mas não ao ponto de saber que iria ser provavelmente minha profissão pro resto dela. Tanto meu ensino médio, quanto a minha graduação são voltadas para a área de TI e ao decorrer da graduação,
                         descobri algo que me brilhou os olhos: programação web. Estou atualmente focado em estudar ReactJS e Typescript.
                     </Text>
                 </Box>
 
-                <Box padding={["6", "8"]} borderRadius='8' pb='4' bg='#101213' h='30vh' ml='4' mr='4'>
-                    <Text fontWeight='extrabold' fontSize='2xl' mb='4' color='#0befda' align='center' fontFamily='Open Sans, sans-serif'>
+                <Box padding={["6", "8"]} borderRadius='8' pb='4' bg='#101213' h='100%' ml='4' mr='4' m='auto 0'>
+                    <Text fontWeight='extrabold' fontSize={["sm", "md", "lg", "2xl"]} mb='12' color='#0befda' align='center' fontFamily='Open Sans, sans-serif'>
                         Habilidades
                     </Text>
 
                     <Flex gap='8' mt='6' wrap='wrap' justifyContent='space-evenly' >
                         <Box display='flex' gap='2' alignItems='center'>
-                            <Image src='chakra-ui-icon.png' boxSize='40px' />
-                            <Text fontWeight='black' fontSize='4xs' color='white' fontFamily='Open Sans, sans-serif'> Chakra UI </Text>
+                            <Image src='chakra-ui-icon.png' boxSize={["15px","20px","30px",'40px']} />
+                            <Text fontWeight='black' fontSize={["xs", "2xs", "3xs", "lg"]} color='white' fontFamily='Open Sans, sans-serif'> Chakra UI </Text>
                         </Box>
                         <Box display='flex' gap='2' alignItems='center'>
-                            <Image src='react-icon.png' boxSize='40px' />
-                            <Text fontWeight='black' fontSize='4xs' color='white' fontFamily='Open Sans, sans-serif'> React </Text>
+                            <Image src='react-icon.png' boxSize={["15px","20px","30px",'40px']} />
+                            <Text fontWeight='black' fontSize={["xs", "2xs", "3xs", "lg"]} color='white' fontFamily='Open Sans, sans-serif'> React </Text>
                         </Box>
                         <Box display='flex' gap='2' alignItems='center'>
-                            <Image src='typescript-icon.png' boxSize='40px' />
-                            <Text fontWeight='black' fontSize='4xs' color='white' fontFamily='Open Sans, sans-serif'> Typescript </Text>
+                            <Image src='typescript-icon.png' boxSize={["15px","20px","30px",'40px']} />
+                            <Text fontWeight='black' fontSize={["xs", "2xs", "3xs", "lg"]} color='white' fontFamily='Open Sans, sans-serif'> Typescript </Text>
                         </Box>
                         <Box display='flex' gap='2' alignItems='center'>
-                            <Image src='vite-logo.png' boxSize='40px' />
-                            <Text fontWeight='black' fontSize='4xs' color='white' fontFamily='Open Sans, sans-serif'> Vite </Text>
+                            <Image src='vite-logo.png' boxSize={["15px","20px","30px",'40px']} />
+                            <Text fontWeight='black' fontSize={["xs", "2xs", "3xs", "lg"]} color='white' fontFamily='Open Sans, sans-serif'> Vite </Text>
                         </Box>
                         <Box display='flex' gap='2' alignItems='center'>
-                            <Image src='git-icon.png' boxSize='40px' />
-                            <Text fontWeight='black' fontSize='4xs' color='white' fontFamily='Open Sans, sans-serif'> Git </Text>
+                            <Image src='git-icon.png' boxSize={["15px","20px","30px",'40px']} />
+                            <Text fontWeight='black' fontSize={["xs", "2xs", "3xs", "lg"]} color='white' fontFamily='Open Sans, sans-serif'> Git </Text>
                         </Box>
                         <Box display='flex' gap='2' alignItems='center'>
-                            <Image src='javascript-icon.png' boxSize='40px' />
-                            <Text fontWeight='black' fontSize='4xs' color='white' fontFamily='Open Sans, sans-serif'> Javascript </Text>
+                            <Image src='javascript-icon.png' boxSize={["15px","20px","30px",'40px']} />
+                            <Text fontWeight='black' fontSize={["xs", "2xs", "3xs", "lg"]} color='white' fontFamily='Open Sans, sans-serif'> Javascript </Text>
                         </Box>
                         <Box display='flex' gap='2' alignItems='center'>
-                            <Image src='bootstrap-icon.png' boxSize='40px' />
-                            <Text fontWeight='black' fontSize='4xs' color='white' fontFamily='Open Sans, sans-serif'> Bootstrap </Text>
+                            <Image src='bootstrap-icon.png' boxSize={["15px","20px","30px",'40px']} />
+                            <Text fontWeight='black' fontSize={["xs", "2xs", "3xs", "lg"]} color='white' fontFamily='Open Sans, sans-serif'> Bootstrap </Text>
                         </Box>
                         <Box display='flex' gap='2' alignItems='center'>
-                            <Image src='scrum-icon.png' boxSize='40px' />
-                            <Text fontWeight='black' fontSize='4xs' color='white' fontFamily='Open Sans, sans-serif'> Scrum </Text>
+                            <Image src='scrum-icon.png' boxSize={["15px","20px","30px",'40px']} />
+                            <Text fontWeight='black' fontSize={["xs", "2xs", "3xs", "lg"]} color='white' fontFamily='Open Sans, sans-serif'> Scrum </Text>
                         </Box>
                     </Flex>
 
