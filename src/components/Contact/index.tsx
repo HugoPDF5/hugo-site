ba422de51d9439f580ae9cc7b4b7abaf1c839383
Hugo <hugopatricio51@gmail.com>
2023-01-06 12:40:50 -0300

    fix: adding responsiveness

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -12,35 +12,35 @@ export const Contact = () => {
                 <Sidebar />
                 <Flex flex='1' direction='column'>
                     <Box h='20%' w='100%'>
-                        <Heading mt='36' textAlign='center' fontFamily='Open Sans' fontWeight='bold' fontSize={'4xl'} color='#0befda'>Entre em contato comigo:</Heading>
+                        <Heading mt={["4","8","16",'36']} textAlign='center' fontFamily='Open Sans' fontWeight='bold' fontSize={["sm", "xl", "2xl", "4xl"]} color='#0befda'>Entre em contato comigo:</Heading>
                     </Box>
-                    <Flex flex='1'>
-                        <HStack color='white' spacing='16' w='100%' align='center' justify='space-evenly'>
-                            <Flex direction='column' align='center' gap='4' >
-                                <Icon as={MdOutlineLocalPhone} boxSize='16' color='#0befda' />
+                    <Flex flex='1' >
+                        <Flex color='white' gap='16' w='100%' mt='8' align='center' justify='space-evenly' direction={['column', 'column' ,'row', 'row']}>
+                            <Flex direction='column' align='center' gap='4'>
+                                <Icon as={MdOutlineLocalPhone} boxSize={['6','8','12','16']} color='#0befda' />
                                 <Text fontFamily='Poppins, sans-serif' fontWeight='bold'>Telefone</Text>
                                 <Text fontFamily='Poppins, sans-serif' fontWeight='semibold'>(88) 99339-5916</Text>
                             </Flex>
                             <Flex direction='column' align='center' gap='4' >
-                                <Icon as={MdOutlineLocalPostOffice} boxSize='16' color='#0befda' />
+                                <Icon as={MdOutlineLocalPostOffice} boxSize={['6','8','12','16']} color='#0befda' />
                                 <Text fontFamily='Poppins, sans-serif' fontWeight='bold'>Email</Text>
                                 <Text fontFamily='Poppins, sans-serif' fontWeight='semibold'>hugopatricio51@gmail.com</Text>
                             </Flex>
                             <Flex direction='column' align='center' gap='4' >
-                                <Icon as={AiFillGithub} boxSize='16' color='#0befda' />
+                                <Icon as={AiFillGithub} boxSize={['6','8','12','16']} color='#0befda' />
                                 <Text fontFamily='Poppins, sans-serif' fontWeight='bold'>GitHub</Text>
                                 <Link href='https://github.com/HugoPDF5' isExternal >
                                     <Text fontFamily='Poppins, sans-serif' fontWeight='semibold'>HugoPDF5 <ExternalLinkIcon mx='2px'/></Text>
                                 </Link>
                             </Flex>
                             <Flex direction='column' align='center' gap='4' >
-                                <Icon as={AiFillLinkedin} boxSize='16' color='#0befda' />
+                                <Icon as={AiFillLinkedin} boxSize={['6','8','12','16']} color='#0befda' />
                                 <Text fontFamily='Poppins, sans-serif' fontWeight='bold'>Linkedin</Text>
                                 <Link href='https://github.com/HugoPDF5' isExternal >
                                 <Text fontFamily='Poppins, sans-serif' fontWeight='semibold'>Hugo Patrício <ExternalLinkIcon mx='2px'/></Text>
                                 </Link>
                             </Flex>
-                        </HStack>
+                        </Flex>
                     </Flex>
                 </Flex>
             </Flex>
